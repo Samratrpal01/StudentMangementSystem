d133d4b8765c4ffcd294050ec2382d55b47f7c77
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2023-12-09 23:42:28 +0530

    Update server.js

--- utils/server.js
+++ utils/server.js
@@ -1,6 +1,1 @@
-const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://jacksparrowrp01:asdfghjkl@cluster0.uaocljz.mongodb.net/?retryWrites=true&w=majority').then(()=>{
-    console.log('connected to DB')
-}).catch(()=>{
-    console.log("Failed in macking Connection")
-})
+
